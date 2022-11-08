7e95289695760c66cb829ecd619b3404ace211a4
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-08 16:00:32 +0100

    password di recupero non sicura (bonus)

--- script.js
+++ script.js
@@ -19,4 +19,18 @@ console. log (
 
 // Password (non sicura)
 
-console. log ("Password: " + nome + cognome + colorePreferito + 21)
+console. log ("Password: " + nome + cognome + colorePreferito + 21);
+
+// Pasword di recupero (non sicura)
+
+let numero1 = prompt ("Inserisci un numero per calcolo password");
+let numero2 = prompt ("Inserisci un altro numero per calcolo password");
+
+numero1 = parseInt (numero1);
+console. log ("Primo numero scelto: " + numero1);
+
+numero2 = parseInt (numero2);
+console. log ("Secondo numero scelto: " + numero2);
+
+let divisione = numero1 / numero2;
+console. log ("Password di recupero:" + nome + cognome + colorePreferito + divisione);
