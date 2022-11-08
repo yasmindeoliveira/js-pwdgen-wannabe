48f948166f807cdf13ce69085fe08476b9d89ad5
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-08 15:14:00 +0100

    create le variabili

---
+++ script.js
@@ -0,0 +1,6 @@
+// Variabili
+
+let nome = prompt ("Inserisci il tuo nome");
+let cognome = prompt ("Inserisci il tuo cognome");
+let colorePreferito = prompt ("Inserisci il tuo colore preferito");
+
