2c804f6f018356fa2abbb8677a389a75d2d146cb
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-08 15:22:39 +0100

    stampa su console

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ let nome = prompt ("Inserisci il tuo nome");
 let cognome = prompt ("Inserisci il tuo cognome");
 let colorePreferito = prompt ("Inserisci il tuo colore preferito");
 
+// Stampa console
+
+console. log (
+    `
+        Nome: ${nome};
+        Cognome: ${cognome};
+        Colore Preferito: ${colorePreferito};
+    `
+)
