859f53a7913ce77c5b0944d3aafee69f3296eefa
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-09 21:39:02 +0100

    codice JS completato

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
-// Variabili
+let passwordGenerator = document.getElementById("password-button");
+
+passwordGenerator.addEventListener('click', function () {
+
+    let nome = document.getElementById("input-nome").value;
+    let cognome = document.getElementById("input-cognome").value;
+    let colore = document.getElementById("input-colore").value;
+    let numero1 = document.getElementById("input-numero1").value;
+    let numero2 = document.getElementById("input-numero2").value;
+
+    let password = nome + cognome + colore + "21";
+
+    numero1 = parseInt(numero1);
+    console.log("Primo numero scelto: " + numero1);
+
+    numero2 = parseInt(numero2);
+    console.log("Secondo numero scelto: " + numero2);
+
+    let divisione = numero1 / numero2;
+    let passwordRecupero = nome + cognome + colore + divisione;
+
+
+    document.getElementById("p-password").innerHTML = password;
+    document.getElementById("p-passwordRecupero").innerHTML = passwordRecupero;
+
+});
+
+
+
+
+//Codice non più valido
+
+/* // Variabili
 
 let nome = prompt ("Inserisci il tuo nome");
 let cognome = prompt ("Inserisci il tuo cognome");
@@ -43,4 +75,4 @@ document.getElementById ("p-colorePreferito") .innerHTML = colorePreferito;
 document.getElementById ("p-numero1") .innerHTML = numero1
 document.getElementById ("p-numero2") .innerHTML = numero2
 document.getElementById ("p-password") .innerHTML = nome + cognome + colorePreferito + 21;
-document.getElementById ("p-passwordRecupero") .innerHTML = nome + cognome + colorePreferito + divisione;
+document.getElementById ("p-passwordRecupero") .innerHTML = nome + cognome + colorePreferito + divisione; */
